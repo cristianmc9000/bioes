e3dbe3f960c8506cd03354435d52abeee90fc15c
rodrigogd1212 <rodrigogonzalesdolz@gmail.com>
2021-08-17 09:09:57 -0400

    login

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,7 +1,8 @@
 <?php 
 
-$conexion = new mysqli('localhost','u604223767_bio','papapapa1212AC!','u604223767_bio'); //BASE HOSTINGER.
-// $conexion = new mysqli('localhost','root','','base_arbell');
+$conexion = new mysqli('bvkp1uqpwrvsuioatujl-mysql.services.clever-cloud.com','umq5o56fqyxs8loy','2hxcnX6DzQoI2W8EgCPT','bvkp1uqpwrvsuioatujl');
+//('localhost','u604223767_bio','papapapa1212AC!','u604223767_bio'); //BASE HOSTINGER.
+
 
 //$conexion = new mysqli('br6vrwqejcpdav29tluj-mysql.services.clever-cloud.com','usz8khcb6vchmslt','1DjIm6ncGfmq6N81hRtt','br6vrwqejcpdav29tluj');
 
