8824cca8225e0477606ea7e29dda8292154bae63
cristianmc9000 <cristian.preof1@gmail.com>
2021-11-29 22:10:37 -0400

    cambios menores

--- recursos/productos/combo_content.php
+++ recursos/productos/combo_content.php
@@ -3,7 +3,7 @@ require("../conexion.php");
 
 $id = $_GET['id'];
 
-$result = $conexion->query("SELECT a.id_prod, b.descripcion FROM combo a, productos b WHERE a.id_combo = ".$id." AND a.id_combo = b.id AND a.estado = 1");
+$result = $conexion->query("SELECT a.id_prod, (SELECT b.descripcion WHERE a.id_prod = b.id) FROM combo a, productos b WHERE a.id_combo = ".$id." AND a.id_prod = b.id AND a.estado = 1");
 $res = $result->fetch_all(MYSQLI_ASSOC); //PARA OBTENER EL ARRAY COMPLETO SIN HACER FOREACH O WHILE
 echo json_encode($res);
 // SELECT a.id_prod, (SELECT b.descripcion WHERE a.id_prod = b.id) FROM combo a, productos b WHERE a.id_combo = "222" AND a.id_prod = b.id AND a.estado = 1
