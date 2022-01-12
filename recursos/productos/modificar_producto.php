72596a5443aaea3d0914294c064e46279258f48d
cristianmc9000 <cristian.preof1@gmail.com>
2022-01-12 16:54:25 -0400

    imagenes hostinger

--- recursos/productos/modificar_producto.php
+++ recursos/productos/modificar_producto.php
@@ -21,11 +21,11 @@ if(strlen($descripcion) > $maxCaracteres) {
 
 /* FALTA AGREGAR BIEN LA RUTA DE LAS IMAGENES */
 if(!empty($archivo)){
-// $ruta = $_SERVER['DOCUMENT_ROOT']."/images/fotos_prod"; //PARA SUBIR AL 000WEBHOST
-$ruta = $_SERVER['DOCUMENT_ROOT']."/bioes/images/fotos_prod"; //imagenes local
-$ruta = $ruta."/".$nombreimg;
-move_uploaded_file($archivo, $ruta);
-$ruta2 = "images/fotos_prod/".$nombreimg;
+	$ruta = $_SERVER['DOCUMENT_ROOT']."/images/fotos_prod"; //PARA SUBIR AL 000WEBHOST
+	// $ruta = $_SERVER['DOCUMENT_ROOT']."/bioes/images/fotos_prod"; //imagenes local
+	$ruta = $ruta."/".$nombreimg;
+	move_uploaded_file($archivo, $ruta);
+	$ruta2 = "images/fotos_prod/".$nombreimg;
 }else{
 	$ruta2 = $imgant;
 }
