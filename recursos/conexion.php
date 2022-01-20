36c70829a6f24b0e9fd52514fbfbdf6cf3d0e20f
cristianmc9000 <cristian.preof1@gmail.com>
2022-01-20 10:45:36 -0400

    conexion

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,10 +1,9 @@
 <?php 
 
-//BASE HOSTINGER.
-$conexion = new mysqli('localhost','u604223767_bio','papapapa1212AC!','u604223767_bio');
 
-//base localhost
-// $conexion = new mysqli('localhost','root','','bio'); 
+$conexion = new mysqli('localhost','u604223767_bio','papapapa1212AC!','u604223767_bio');//BASE HOSTINGER.
+
+// $conexion = new mysqli('localhost','root','','bio'); //base localhost
 
 //$conexion = new mysqli('br6vrwqejcpdav29tluj-mysql.services.clever-cloud.com','usz8khcb6vchmslt','1DjIm6ncGfmq6N81hRtt','br6vrwqejcpdav29tluj');
 
