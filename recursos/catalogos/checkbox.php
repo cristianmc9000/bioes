410bc7efc4032cf959e8dc2101140a42b919f96c
cristianmc9000 <cristian.preof1@gmail.com>
2022-08-22 17:53:00 -0400

    agregando comillas a checkbox.php

--- recursos/catalogos/checkbox.php
+++ recursos/catalogos/checkbox.php
@@ -2,7 +2,7 @@
 	require("../conexion.php");
 	
 	$id = $_GET['id'];
-	$result = $conexion->query('SELECT checkbox FROM productos WHERE id = '.$id);
+	$result = $conexion->query('SELECT checkbox FROM productos WHERE id = "'.$id.'"');
 	$res = $result->fetch_all(MYSQLI_ASSOC);
 
 	echo $res[0]['checkbox'];
